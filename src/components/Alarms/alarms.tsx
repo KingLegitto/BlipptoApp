bfed1b82084ab33d23706d346e8d75e6b61a0d25
Chibuike Amadi <35205887+amadijeffrey@users.noreply.github.com>
2023-10-21 23:35:26 +0100

    Feature/implement active alarms (#29)
    * implement activate alarm section
    
    * completed activat alarm component
    
    * worked on the active alarm history component
    
    * made the dummy data more realistic

--- src/components/Alarms/alarms.tsx
+++ src/components/Alarms/alarms.tsx
@@ -9,6 +9,9 @@ import { ReactComponent as RobberyAlarmIcon } from "../../assets/robberyAlarmIco
 import { ReactComponent as ThreatAlarmIcon } from "../../assets/threatAlarmIcon.svg";
 import { ReactComponent as OtherAlarmIcon } from "../../assets/otherAlarmIcon.svg";
 import { ReactComponent as QuestionMarkIcon } from "../../assets/questionMark.svg";
+import { activeAlarmData } from "../../dummydata/alarmDummyData";
+import { ReactComponent as GridView } from "../../assets/gridView.svg";
+
 
 const Alarms = () => {
   const navigate = useNavigate();
@@ -88,6 +91,57 @@ const Alarms = () => {
         </div>
         <div className="flex flex-col w-3/5">
           <div className="bg-white h-[40vh] rounded-2xl mb-5"></div>
+          <div className="bg-white h-[45vh] rounded-2xl">
+            <div className="p-5 border-b-2 flex items-center justify-between h-[18%]">
+              <h4 className="font-semibold text-lg">Active Alarms</h4>
+              <div className="h-8 md:h-10 w-8 md:w-10 bg-brand rounded-md  flex justify-center items-center">
+                <GridView className="scale-[0.7] xl:scale-75" />
+              </div>
+            </div>
+            <div className="px-5 pb-5 max-h-[81%] overflow-y-auto activeAlarms box-border">
+              <table className="w-full table-fixed text-left">
+                <thead>
+                  <tr className="h-9 md:h-14 border-b sticky top-0 z-10 bg-white">
+                    <th className="text-xs md:text-sm w-1/4">Alarm Type</th>
+                    <th className="text-xs md:text-sm w-1/4">Resident</th>
+                    <th className="text-xs md:text-sm w-1/4 hidden md:table-cell">
+                      Apartment Address
+                    </th>
+                    <th className="text-xs md:text-sm w-1/4">
+                      Current Location
+                    </th>
+                    <th></th>
+                  </tr>
+                </thead>
+                <tbody className="">
+                  {activeAlarmData.map(
+                    (
+                      {
+                        resident,
+                        apartmentAddress,
+                        currentLocation,
+                        alarmType,
+                      },
+                      i
+                    ) => {
+                      return (
+                        <tr key={i} className="h-9 md:h-12 border-b">
+                          <td className="text-xs md:text-sm ">{alarmType}</td>
+                          <td className="text-xs md:text-sm ">{resident}</td>
+                          <td className="text-xs md:text-sm  hidden md:table-cell">
+                            {apartmentAddress}
+                          </td>
+                          <td className="text-xs md:text-sm hidden lg:table-cell ">
+                            {currentLocation}
+                          </td>
+                        </tr>
+                      );
+                    }
+                  )}
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
       </div>
     </div>
---
+++ src/dummydata/alarmDummyData.js
@@ -0,0 +1,45 @@
+export const activeAlarmData = [
+    {
+      alarmType: "Medical",
+      resident: "John Michael",
+      apartmentAddress: "23, Lantana road",
+      currentLocation: "23, Lantana road",
+    },
+    {
+      alarmType: "Threat",
+      resident: "David Peterson",
+      apartmentAddress: "12c, Izora road",
+      currentLocation: "12c, Izora road",
+    },
+    {
+      alarmType: "Robbery",
+      resident: "David Cornerstone",
+      apartmentAddress: "23, Jasmine road",
+      currentLocation: "23, Jasmine road",
+    },
+    {
+      alarmType: "Medical",
+      resident: "John Michael",
+      apartmentAddress: "23b, Alaiyenlunwa Oguntere",
+      currentLocation: "23b, Alaiyenlunwa Oguntere",
+    },
+    {
+      alarmType: "Threat",
+      resident: "John Michael",
+      apartmentAddress: "23, Lantana road",
+      currentLocation: "23, Lantana road",
+    },
+    {
+        alarmType: "Medical",
+        resident: "John Schedtmann",
+        apartmentAddress: "23, Lantana road",
+        currentLocation: "23, Lantana road",
+      },
+      {
+        alarmType: "Threat",
+        resident: "John Lumpkin",
+        apartmentAddress: "21, Jasmine road",
+        currentLocation: "21, Jasmine road",
+      },
+   
+  ];
